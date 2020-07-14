193d4a8a0423b3c515a61f7779ccd874e1882d08
muzz56 <muzammil0526@gmail.com>
2020-07-14 16:29:27 -0400

    Lab2

---
+++ test/test-bitwise.js
@@ -0,0 +1,35 @@
+const BitWise = artifacts.require('BitWise');
+const { BN } = require('@openzeppelin/test-helpers');
+const chaiBN = require('chai-bn')(BN); 
+require('chai').use(chaiBN);
+const { expect } = require('chai');
+
+contract("BitWise", () =>{
+    let bitwise;
+   before(() => {
+        return BitWise.deployed().then(instance => {
+            bitwise = instance;
+        })
+    })
+
+    it('countBitSet() should pass', async () => {
+        const myNumber = 250;
+        const gas = await bitwise.countBitSet.estimateGas(myNumber);
+        const gasAsm = await bitwise.countBitSetAsm.estimateGas(myNumber);
+        expect(gas).to.be.gt(gasAsm, "Assembly should be more gas efficient");
+
+        const result = await bitwise.countBitSet(myNumber);
+        const resultAsm = await bitwise.countBitSetAsm(myNumber);
+        expect(result).to.be.a.bignumber.that.equal(resultAsm, "result should match");
+    })
+
+    it('countBitSet() should pass', async () => {
+        const myNumber = 0;
+        const result = await bitwise.countBitSet(myNumber);
+        const resultAsm = await bitwise.countBitSetAsm(myNumber);
+        assert.equal(result, myNumber, "result should match");
+        assert.equal(resultAsm, myNumber, "result should match");
+        expect(result).to.be.a.bignumber.that.equal(resultAsm, "result should match");
+    })
+    
+}) 
